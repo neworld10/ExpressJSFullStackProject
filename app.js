f361c90206d40323111be1ba37bb8c8d80f6d707
neworld10 <82375920+neworld10@users.noreply.github.com>
2025-05-07 19:34:40 +0900

    put 개뻘짓

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-import express from 'express';
+import express, { request } from 'express';
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var data = {
+    name: 'neworld'
+}
 
 app.get('/', (request, response) => {
     return response.send('Hello World!');
 });
 
+app.post('/users', (request, response) => {
+    const name = request.body.name;
+    const email = request.body.email;
+
+    return response.send(`${name} ${email}`);
+});
+
+app.put('/users/:name', (request, response) => {
+    const name = request.params.name;
+    const email = request.body.email;
+
+    const target = data.name === name ? data : null;
+    if (target) {
+        target.email = email;
+        return response.send(target);
+    } else {
+        return response.status(404).send('User not found');
+    }
+})
+
 app.listen(port, () => {
-    console.log('Example app listen on port ${port})');
+    console.log(`Example app listen on port ${port}`);
 });
